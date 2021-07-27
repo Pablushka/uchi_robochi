614d1bc22eb924bf7cad86744dc2980bfde05b92
Pablo Centurión <pablushka@nomades.com.ar>
2021-07-27 07:21:23 -0300

    Initial Commit

---
+++ uchi_robochi/uchi_robochi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for uchi_robochi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uchi_robochi.settings')
+
+application = get_wsgi_application()
