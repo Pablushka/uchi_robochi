55747a8fac2341e627c3d9086488a536ec9f201b
Pablo Centurión <pablushka@nomades.com.ar>
2021-10-26 21:38:07 -0300

    The tests are beautiful

--- uchi_robochi/test/pi_commander_models_test.py
+++ uchi_robochi/test/pi_commander_models_test.py
@@ -6,4 +6,12 @@ from pi_commander import models
 @pytest.mark.django_db(transaction=True)
 def test_create_raspberry():
     factories.RaspberryFactory()
+
     assert models.Raspberry.objects.count() is 1
+
+
+@pytest.mark.django_db(transaction=True)
+def test_check_raspy_name():
+    factories.RaspberryFactory()
+
+    assert models.Raspberry.objects.first().name == "Pepe"
