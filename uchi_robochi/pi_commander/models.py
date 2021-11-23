d5a2f31014710dfd1151616ac09fe0ec51960aa7
Pablo Centurión <pablushka@nomades.com.ar>
2021-11-23 22:56:57 +0000

    Added descriptio to model relay
    Co-authored-by: Rocio <rochimo2@users.noreply.github.com>
    Co-authored-by: Lablonda <Lablonda@users.noreply.github.com>
    Co-authored-by: Noja37 <Noja37@users.noreply.github.com>
    Co-authored-by: Gonzalo Dominguez <dgonnzalo@users.noreply.github.com>
    Co-authored-by: Victoria Carbone <vic7oria@users.noreply.github.com>

--- uchi_robochi/pi_commander/models.py
+++ uchi_robochi/pi_commander/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
+TIME_UNIT_OPTIONS = [("D", "Days"), ("H", "Hours"), ("M", "Minutes"), ("S", "Seconds")]
+
 
 class Raspberry(models.Model):
     name = models.CharField(max_length=100, default="Raspberry 1")
@@ -15,15 +17,16 @@ class Raspberry(models.Model):
 class Relay(models.Model):
     label = models.CharField(max_length=40, default="Relay 1")
     raspberry = models.ForeignKey(Raspberry, on_delete=CASCADE)
-    description = models.CharField(max_length=250, default="")
 
     def __str__(self):
-        return f"{self.label}: {self.description}"
+        return f"{self.label}"
 
 
 class Action(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=CASCADE)
     raspberry = models.ForeignKey(Raspberry, on_delete=CASCADE)
     relay = models.ForeignKey(Relay, on_delete=CASCADE)
     status = models.BooleanField(default=False)
+    time_out = models.IntegerField(blank=True, null=True)
+    time_unit = models.CharField(max_length=1, choices=TIME_UNIT_OPTIONS, default="M")
